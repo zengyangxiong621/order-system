11d4b2a234911dcb2ba1a1793aad87de86308ca7
zengyangxiong621 <66625800+zengyangxiong621@users.noreply.github.com>
2024-01-11 09:46:43 +0800

    Update request.js

--- src/network/request.js
+++ src/network/request.js
@@ -1,9 +1,20 @@
 import axios from 'axios'
+let baseURL = 'http://127.0.0.1'
+let port = 3000
 
+if (process.env.NODE_ENV === 'production') {
+  baseURL = process.env.BASE_URL
+  port = process.env.PORT
+  console.log('This is production environment');
+} else if (process.env.NODE_ENV === 'development') {
+  console.log('This is development environment');
+} else {
+  console.log('This is neither production nor development environment');
+}
 export function request (config) {
   // 1.创建 axios 的实例
   const instance = axios.create({
-    baseURL: 'http://127.0.0.1:3000',
+    baseURL: baseURL+':'+port,
     timeout: 5000
   })
   // 2.axios 的拦截器
